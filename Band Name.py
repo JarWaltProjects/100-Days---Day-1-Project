1f0138ecd51bced709847fd9e71710f9b650a2c4
Jarid Walters <132961227+JarWaltProjects@users.noreply.github.com>
2024-06-24 14:16:36 -0400

    Code for generating a band name based on input

---
+++ Band Name.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in? \n")
+pet_name = input("What's your pets name? \n")
+print("Your band name is " + city + " " + pet_name + "!" )
